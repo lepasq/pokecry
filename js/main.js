8f030102e8299f78930f0e27a8276fb7e64a7ecd
lepasq <53230128+lepasq@users.noreply.github.com>
2020-01-02 11:46:29 +0100

    added dropdown menu for the current battle theme

--- js/main.js
+++ js/main.js
@@ -14,3 +14,27 @@ function randomCry() {
   console.log(random);
   audio.play();
 }
+
+function selectBattle(bt) {}
+
+function battleTheme() {}
+
+function myDrop() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+        document.getElementById("current-text").innerHTML = battle;
+        myAudio.pause();
+      }
+    }
+  }
+};
